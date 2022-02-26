5409928afae78321a91399110eedf89790d0bafc
Dann38 <38dunn@gmail.com>
2022-02-26 14:52:21 +0800

    Добавили выпадающее меню пользователя

--- src/entities/NavigationBar.js
+++ src/entities/NavigationBar.js
@@ -1,10 +1,17 @@
 import React from "react"
+import OpenWindow from '../user/UserWindow';
 
 const NavigationBar = () => (
     <nav className="nav-bar">
         <a href="#" className="link">ссылка куда-то</a>
         <a href="#" className="link">ссылка куда-то</a>
         <a href="#" className="link">ссылка куда-то</a>
+        <div className="block-user-window">
+            <div className="user-window">
+                Макс пока свой код тут будет писать
+            </div>
+            <div className="user-window-link" onClick={OpenWindow}></div>
+        </div>
    </nav>
 );
 
---
+++ src/user/UserWindow.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function OpenWindow(e) {
+    var classNameNow = e.target.parentElement.children[0].className;
+    if (classNameNow == "user-window"){
+        e.target.parentElement.children[0].className ="user-window-hidden"
+        classNameNow = "user-window-hidden"
+    }else if (classNameNow == "user-window-hidden"){
+        e.target.parentElement.children[0].className ="user-window"
+    }
+}
+
+export default OpenWindow;
