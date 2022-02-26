c51202f1e468407a461a9f54652e67755068d47d
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-02-26 15:13:10 +0800

    Update equals, delete react import

--- src/user/UserWindow.js
+++ src/user/UserWindow.js
@@ -1,11 +1,9 @@
-import React from "react"
-
 function OpenWindow(e) {
     var classNameNow = e.target.parentElement.children[0].className;
-    if (classNameNow == "user-window"){
+    if (classNameNow === "user-window"){
         e.target.parentElement.children[0].className ="user-window-hidden"
         classNameNow = "user-window-hidden"
-    }else if (classNameNow == "user-window-hidden"){
+    }else if (classNameNow === "user-window-hidden"){
         e.target.parentElement.children[0].className ="user-window"
     }
 }
