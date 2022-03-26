ae32f5e1896d4f45c13be2d3f8fe99c2b789bdb5
MakcReaper <99306959+MakcReaper@users.noreply.github.com>
2022-03-26 14:12:38 +0800

    Заготовка для proxi

---
+++ setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:5000',
+      changeOrigin: true,
+    })
+  );
+};
+"http-proxy-middleware": "^2.0.4",
