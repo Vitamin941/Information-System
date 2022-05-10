ccf673929ba6ff0b0ff461060337e8b6d0e062cf
Dann38 <38dunn@gmail.com>
2022-05-10 13:30:48 +0800

    Сделал отрисовку кружков

--- src/logic/Question.js
+++ src/logic/Question.js
@@ -1,16 +1,18 @@
 import React from "react";
 import axios from "axios";
+// import { For } from 'react-for';
 
 export class Question extends React.Component {
     constructor(props) {
         super()
 
         let clicked = props.clicked ? true : false
-
+        
         this.state = {
             text: props.text,
             id: props.id,
-            clicked: clicked
+            clicked: clicked,
+            level: props.level
         }
 
         this.HandleClick = this.HandleClick.bind(this)
@@ -18,7 +20,20 @@ export class Question extends React.Component {
         this.HandleSubmit = this.HandleSubmit.bind(this)
 
     }
-
+    getCircle (level) {
+        let div = []
+        let circle = "circle"
+        let circleEmpty = "circle-empty"
+        for (let i = 1; i <= 5; i++){
+            if (i <= level){
+                div.push(circle)
+            } else {
+                div.push(circleEmpty)
+            }       
+        }
+        console.log(div)
+        return div
+    }
     HandleClick = (e) => {
         e.preventDefault()
         console.log("Начал редактировать")
@@ -93,11 +108,26 @@ export class Question extends React.Component {
                     <p className="question-text">{this.state.text}</p>
                 </div>
             }
-            <div className="circle-empty"/>   
+            {this.getCircle(this.state.level).map(type_ => {
+                return <div className={type_}/>
+            })
+
+            }
+            {/* <For start={1} comparator={i => i<=5} next={i => i++}>{
+                (i) => {
+                    {i < this.level 
+                        ?
+                        <div className="circle-empty"/> 
+                        :
+                        <div className="circle"/>
+                    }
+                }
+            }</For> */}
+            {/* <div className="circle-empty"/>   
             <div className="circle-empty"/>
             <div className="circle-empty"/>
             <div className="circle"/>
-            <div className="circle"/>
+            <div className="circle"/> */}
             <button onClick={() => this.props.deleteItem()}>Удалить ({this.state.id})</button>
             <button onClick={e => this.updateQuestion(e)}>Обновить ({this.state.id})</button>
             <button onClick={e => this.getAnswer(e)}>Показать ответ({this.state.id})</button>
